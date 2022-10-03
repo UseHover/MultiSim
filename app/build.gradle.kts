30e0c9c047fb011b0184cb0bad6eb57b404d6e8d
Juma Allan <allanjuma@gmail.com>
2022-10-03 21:32:42 +0300

    migrate to versions plugin and cleaning up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,12 +44,12 @@ dependencies {
 
     implementation(project(":multisim"))
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation(libs.android.coreKtx)
+    implementation(libs.android.appcompat)
+    implementation(libs.android.material)
 
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.6.1")
+    androidTestImplementation(libs.test.android.junit)
+    androidTestImplementation(libs.test.android.espresso)
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    testImplementation(libs.test.junit4)
 }
--- multisim/build.gradle.kts
+++ multisim/build.gradle.kts
@@ -30,16 +30,14 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    namespace = "com.hover.multisim"
 
     dependencies {
-        implementation("androidx.appcompat:appcompat:1.5.1")
-        implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
-        implementation("androidx.work:work-runtime:2.7.1")
+        implementation(libs.android.appcompat)
+        implementation(libs.android.localbroadcastmanager)
+        implementation(libs.android.work)
+        implementation(libs.sentry)
 
-        testImplementation("junit:junit:4.13.2")
-        androidTestImplementation("androidx.test.ext:junit:1.1.3")
-        androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-
-        implementation("io.sentry:sentry-android:6.4.2")
+        testImplementation(libs.test.junit4)
     }
 }
